0dfe72f609d57f671dfd05f3f19aca7eafb8077c
ses9892 <ses1238@gmail.com>
2021-08-20 17:00:33 +0900

    docker file pull

--- src/main/java/com/store/project/application/util/FileUploadBinary.java
+++ src/main/java/com/store/project/application/util/FileUploadBinary.java
@@ -18,6 +18,7 @@ public class FileUploadBinary {
     private final String windowUploadPath = Paths.get("D:","shopImg").toString();
     String test111;
     String filebase64;
+    String test2222;
 
     private final String getRandomString(){
         return UUID.randomUUID().toString().replace("-","");
